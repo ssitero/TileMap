f3daf50aeb79c1433bdb343c36fedc955cbf8459
Samuel <ssitero@knights.ucf.edu>
2018-10-16 19:19:15 -0400

    ..

---
+++ TileMap_U/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;      
+    private Vector3 offset;         
+
+
+    void Start () 
+    {
+
+        player.position.x
+    }
+    
+
+    void LateUpdate () 
+    {
+
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
