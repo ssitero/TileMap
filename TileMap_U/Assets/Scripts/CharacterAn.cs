0b7f3220a4782ca29b77f5a35098af17f1bb2b59
Samuel <ssitero@knights.ucf.edu>
2018-10-16 18:04:14 -0400

    Redid player

---
+++ TileMap_U/Assets/Scripts/CharacterAn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAn : MonoBehaviour {
+
+    private Animator an;
+
+    void Start () {
+        an = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)){
+            an.SetBool("IsRunning", true);
+        } else {
+            an.SetBool("IsRunning", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            an.SetTrigger("Jump");
+        }
+	}
+}
