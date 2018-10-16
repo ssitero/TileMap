b1321e5d7e82d5f348540e4a7b72d05f00e818c4
Samuel <ssitero@knights.ucf.edu>
2018-10-16 19:27:17 -0400

    .

--- TileMap_U/Assets/Scripts/CameraController.cs
+++ TileMap_U/Assets/Scripts/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour {
     void Start () 
     {
 
-        player.position.x
+        transform.position = new Vector3(14.88f, -2.4f, 0.0f);
     }
     
 
