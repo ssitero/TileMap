5d12f3b72fc6ff9ca1f0b5bab5294d6453dca0ef
Samuel <ssitero@knights.ucf.edu>
2018-10-17 15:00:59 -0400

    fixed alot

--- TileMap_U/Assets/Scripts/CameraController.cs
+++ TileMap_U/Assets/Scripts/CameraController.cs
@@ -4,20 +4,22 @@ using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player;      
+    public Transform player;      
     private Vector3 offset;         
 
 
     void Start () 
     {
 
-        transform.position = new Vector3(14.88f, -2.4f, 0.0f);
+        //transform.position = new Vector3(14.88f, -2.4f, 0.0f);
+        offset = transform.position - player.transform.position;
+        transform.position = player.transform.position + offset;
     }
     
 
     void LateUpdate () 
     {
 
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        this.gameObject.transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
     }
 }
