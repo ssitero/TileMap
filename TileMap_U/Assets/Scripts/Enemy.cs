a654fb0f384fbca55c1a186fc8dd1fd80ce28080
Samuel <ssitero@knights.ucf.edu>
2018-10-19 16:09:35 -0400

    lot of work

---
+++ TileMap_U/Assets/Scripts/Enemy.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+
+    public float speed;
+    public float jumpforce;
+
+    //ground check
+    private bool isGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    private bool wallHit;
+    public float wallHitBox;
+    public float wallHitWidth;
+    public float wallHitHeight;
+
+    // private float jumpTimeCounter;
+    //public float jumpTime;
+    //private bool isJumping;
+
+    //audio stuff
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Awake()
+    {
+
+        // source = GetComponent<AudioSource>();
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        // rb2d.AddForce(movement * speed);
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        isGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        Debug.Log(isGround);
+
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+
+
+        //stuff I added to flip my character
+        if (facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isGround)
+        {
+
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                // rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+                rb2d.velocity = Vector2.up * jumpforce;
+
+                wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+                if (wallHit == true)
+                {
+                    speed = speed * -1;
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
+
